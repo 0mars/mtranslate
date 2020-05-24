6e3d9a62c36e3a9c0f6eed8607c9e8cacc01f0a5
Omar Shaban <omars@php.net>
2020-05-24 20:42:29 +0200

    proxy translate

--- mtranslate/core.py
+++ mtranslate/core.py
@@ -79,17 +79,20 @@ def translate(to_translate, to_language="auto", from_language="auto"):
     base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
     proxy_index = random.randint(0, len(ip_addresses) - 1)
     proxy = {"http": ip_addresses[proxy_index]}
-    proxy = urllib2.ProxyHandler(proxy)
-    opener = urllib2.build_opener(proxy)
-    urllib2.install_opener(opener)
     if (sys.version_info[0] < 3):
         to_translate = urllib.quote_plus(to_translate)
         link = base_link % (to_language, from_language, to_translate)
+        proxy = urllib2.ProxyHandler(proxy)
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
         request = urllib2.Request(link, headers=agent)
         raw_data = urllib2.urlopen(request).read()
     else:
         to_translate = urllib.parse.quote(to_translate)
         link = base_link % (to_language, from_language, to_translate)
+        proxy = urllib.ProxyHandler(proxy)
+        opener = urllib.build_opener(proxy)
+        urllib2.install_opener(opener)
         request = urllib.request.Request(link, headers=agent)
         raw_data = urllib.request.urlopen(request).read()
     data = raw_data.decode("utf-8")
