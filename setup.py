76395f9c5bbd263275d8933db2919ae67ecbd2a7
Omar Shaban <omars@php.net>
2020-05-25 12:07:42 +0200

    anythin

--- setup.py
+++ setup.py
@@ -2,16 +2,12 @@ from distutils.core import setup
 import os
 import sys
 
-data_dir = os.path.join(sys.prefix, "local/lib/python3.7/dist-package/mtranslate")
 setup(
     name='mtranslate',
     packages=['mtranslate'],
     package_data={
-        "": ["*.txt", "*.rst"],
-        "mtranslate": ["*.txt"],
+        'static': ['*'],
     },
-    data_files=[("mtranslate", [os.path.join(data_dir, "mydata")])],
-    include_package_data=True,
     version='1.6',
     description='Google translate console script with easy to use API',
     author='Arnaud Alies',
