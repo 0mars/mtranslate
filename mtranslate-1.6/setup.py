695388e15ea0e032f26bb4d092f467b5182d7421
Omar Shaban <omars@php.net>
2020-05-25 08:21:54 +0200

    fix manifest

--- mtranslate-1.6/setup.py
+++ mtranslate-1.6/setup.py
@@ -2,12 +2,21 @@ from distutils.core import setup
 import os
 
 
+def gen_data_files(*dirs):
+    results = []
+
+    for src_dir in dirs:
+        for root,dirs,files in os.walk(src_dir):
+            results.append((root, map(lambda f:root + "/" + f, files)))
+    return results
+
 setup(
     name='mtranslate',
     packages=['mtranslate'],
     package_data={
-        'mtranslate': ['proxy.txt'],
+        'mtranslate': ['mtranslate/*.txt'],
     },
+    data_files=gen_data_files("data"),
     version='1.6',
     description='Google translate console script with easy to use API',
     author='Arnaud Alies',
